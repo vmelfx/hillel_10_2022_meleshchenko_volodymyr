e317afac441c377dcf8f9b03e05402ae19e6f0e6
vmelfx <vladimir.meleshchenko@outllok.com>
2022-11-28 22:23:05 +0200

    Finish Lesson 08 recap
    Finish second example from the lesson 08

--- Lesson_08/on_lesson_08_part_2.py
+++ Lesson_08/on_lesson_08_part_2.py
@@ -16,7 +16,7 @@ class Product:
 
 def authorize_in_paypal(user: User) -> UUID:
     token: UUID = PayPalAPI.get_token()
-    print(f"Authorized with PayPal...\nToken={token}")
+    print(f"Authorized with PayPal...\n\tToken={token}")
     return token
 
 
@@ -34,10 +34,13 @@ def checkout_with_stripe(user: User, product: Product):
 def main():
     john = User(name="John")
     microphone = Product(name="Razer", price=1000)
+    payment_type = input("Enter payment type: ")
 
-    paypal_token: UUID = authorize_in_paypal(john)
-
-    checkout_with_paypal(paypal_token, john, microphone)
+    if payment_type == "paypal":
+        paypal_token: UUID = authorize_in_paypal(john)
+        checkout_with_paypal(paypal_token, john, microphone)
+    elif payment_type == "stripe":
+        pass
     # pay_with_stripe(john, microphone)
 
 
--- Lesson_08/providers.py
+++ Lesson_08/providers.py
@@ -6,8 +6,16 @@ class PayPalAPI:
     def get_token() -> UUID:
         return uuid4()
 
+    @staticmethod
+    def buy(product_name: str, token: UUID) -> UUID:
+        return uuid4()
+
 
 class StripeAPI:
     @staticmethod
     def get_token() -> UUID:
+        return uuid4()
+
+    @staticmethod
+    def checkout(product_name: str, price: float, token: UUID) -> UUID:
         return uuid4()
